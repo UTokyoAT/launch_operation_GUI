8cf215d64a74f1ac0c33287a827959e078ddb238
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2024-10-24 18:27:34 +0900

    [update]senderとreceiverの型引数を関数へ移動

--- src/traits.rs
+++ src/traits.rs
@@ -7,12 +7,12 @@ pub trait Sendable {
 }
 
 ///データを送信する型に実装するトレイト
-pub trait Sender<T: Sendable, E> {
-    fn send(&mut self, data: T) -> Result<(), E>;
+pub trait Sender<E> {
+    fn send<T : Sendable>(&mut self, data: T) -> Result<(), E>;
 }
 
 ///データを受信する型に実装するトレイト
-pub trait Receiver<T: Sendable, E> {
+pub trait Receiver<E> {
     ///データを受信していればそのデータを，していなければエラーを返す
-    fn try_receive(&mut self) -> Result<T, E>;
+    fn try_receive<T : Sendable>(&mut self) -> Result<T, E>;
 }
