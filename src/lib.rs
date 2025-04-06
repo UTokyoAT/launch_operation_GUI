1b765eb0b5e79213b8dcea6ca5eeadec710d673f
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-06 09:23:44 +0900

    [add] template_parser

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod code_generation {
     pub mod code_generation_context;
     pub mod var_type;
     pub mod code_generator;
+    pub mod template_parser;
 }
 pub mod integration_test;
 pub mod serial_communication;
