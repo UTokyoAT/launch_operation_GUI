66aa2573782a358d030c417f3b96b63a539998a1
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-06 11:56:20 +0900

    [add] 生成されたコードのテストを生成する機能を追加

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@ pub mod code_generation {
     pub mod code_generator;
     pub mod template_parser;
 }
+pub mod generated_code {
+    pub mod rust {
+        pub mod command;
+        pub mod log;
+    }
+}
 pub mod integration_test;
 pub mod serial_communication;
 pub mod service;
