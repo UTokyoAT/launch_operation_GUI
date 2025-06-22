77e312c3ea7ad4a0f8da2850bef966feb596b23e
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-06-22 19:47:32 +0900

    [add]テスト機能のPC側を追加

--- test_arduino/test_serial_echo/src/main.rs
+++ test_arduino/test_serial_echo/src/main.rs
@@ -1,3 +1,23 @@
+use test_serial_echo::Log::Log;
+use launch_operation_gui::serial_communication::{SerialSender, SerialReceiver};
+use launch_operation_gui::traits::{Receiver, Sender};
+use std::io;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the port name: ");
+    let log = Log {
+        var1: 1.0,
+        var2: 2.0,
+    };
+    let mut port = String::new();
+    io::stdin().read_line(&mut port).unwrap();
+    port = port.trim().to_string();
+    let mut sender = SerialSender::new(port.clone(), 115200, Duration::from_secs(1));
+    sender.send(log);
+    let mut receiver = SerialReceiver::new(port.clone(), 115200, Duration::from_secs(1));
+    let received_log : Log = receiver.try_receive().unwrap();
+
+    assert_eq!(received_log.var1, 1.0);
+    assert_eq!(received_log.var2, 2.0);
 }
