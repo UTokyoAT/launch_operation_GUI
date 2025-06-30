7126f5c55906ee0d55b7f0fae3f2a2092e9710a9
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-06-30 21:23:54 +0900

    [fix] arduinoとの通信ができるように修正

--- test_arduino/test_serial_echo/src/main.rs
+++ test_arduino/test_serial_echo/src/main.rs
@@ -1,8 +1,10 @@
 use test_serial_echo::Log::Log;
-use launch_operation_gui::serial_communication::{SerialSender, SerialReceiver};
+use launch_operation_gui::serial_communication;
 use launch_operation_gui::traits::{Receiver, Sender};
 use std::io;
 use std::time::Duration;
+use std::thread::sleep;
+use launch_operation_gui::traits::Sendable;
 
 fn main() {
     println!("Enter the port name: ");
@@ -13,11 +15,10 @@ fn main() {
     let mut port = String::new();
     io::stdin().read_line(&mut port).unwrap();
     port = port.trim().to_string();
-    let mut sender = SerialSender::new(port.clone(), 115200, Duration::from_secs(1));
-    sender.send(log);
-    let mut receiver = SerialReceiver::new(port.clone(), 115200, Duration::from_secs(1));
+    let (mut sender, mut receiver) = serial_communication::new_pair(&port, &port, 115200, Duration::from_secs(1)).unwrap();
+    sleep(Duration::from_millis(2000));
+    sender.send(log).unwrap();
     let received_log : Log = receiver.try_receive().unwrap();
-
-    assert_eq!(received_log.var1, 11.0);
-    assert_eq!(received_log.var2, 22.0);
+    assert_eq!(received_log.var1, 1.0);
+    assert_eq!(received_log.var2, 2.0);
 }
