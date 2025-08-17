0e8d2aadc4f1364946e49c2894103887f6029507
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-08-17 15:42:22 +0900

    [fix] cliantが実行できるように修正

--- cliant/run.sh
+++ cliant/run.sh
@@ -2,8 +2,17 @@
 
 set -e  # エラー時にスクリプトを停止
 
+echo "🚀 Running code_generator..."
 cargo run --manifest-path ../code_generator/Cargo.toml ../cliant/config_for_code_generation.txt src/generated_code
 echo "✅ Code generation completed!"
+
+echo "🚀 Running tests..."
+cargo test
+echo "✅ Test completed!"
+
+echo "🚀 Running cliant..."
+cargo run
+
 
 # 必要に応じてcliantも実行
 # echo "🚀 Running cliant..."
